4cd51add09d96ecf4c062a4430f2731bd37dadae
Sameer <schandra7770@gmail.com>
2026-01-07 10:18:08 +0530

    Added middleware for user input validator

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI,Request,Form,Depends
+from fastapi import FastAPI,Request,HTTPException,Form,Depends
 from fastapi.responses import HTMLResponse,RedirectResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
-import database
+import database,re
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -14,6 +14,18 @@ def get_db():
     finally:
         db.close()
 
+def validate_user_data(email:str, password:str):
+    email_regex = r'^\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    if not re.match(email_regex,email):
+        return "Invalid email format"
+    
+    if len(password) < 8:
+        return "Password must be at least 8 characters long"
+    if not any(char.isdigit() for char in password):
+        return "Password must contain at least one number."
+    
+    return None
+
 @app.get("/",response_class=HTMLResponse)
 async def read_form(request: Request):
     return templates.TemplateResponse("home_page.html",{"request":request})
@@ -23,10 +35,17 @@ async def login_form(request: Request):
     return templates.TemplateResponse("login_page.html", {"request": request})
 
 @app.post("/submit")
-async def handle_form(email: str = Form(...),
-                  password: str = Form(...),
-                  db: Session = Depends(get_db)
+async def handle_form(
+    request: Request,    
+    email: str = Form(...),
+    password: str = Form(...),
+    db: Session = Depends(get_db)
 ):
+    error_message = validate_user_data(email,password)
+
+    if error_message:
+        return templates.TemplateResponse("login_page.html",{"request" : request, "error" : error_message})
+
     new_entry = database.UserInput(
         user_email = email,
         user_password = password
